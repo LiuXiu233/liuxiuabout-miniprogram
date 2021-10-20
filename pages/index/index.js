dfe59909f2446607a6114682b3dd25fc4d68b2b8
刘秀 <i@xiu.buzz>
2021-10-20 15:11:42 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,19 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Liuxiu’s about',
+  },
+  copyEmail() {
+    wx.setClipboardData({
+      data: 'i@xiu.buzz',
+    });
+  },
+  copyBlog() {
+    wx.setClipboardData({
+      data: 'https://liuxiu233.com',
+    });
+  },
+})
